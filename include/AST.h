ab091afaae085fbc51e4f5618cdd9ee1378f6588
Binesh <binesh@Bineshs-MacBook-Air.local>
2025-10-25 12:18:29 +0530

    Add Body to Then and Else in IF

--- include/AST.h
+++ include/AST.h
@@ -95,11 +95,12 @@ public:
 
 /// IfExprAST - Expression class for if/then/else.
 class IfExprAST : public ExprAST {
-  std::unique_ptr<ExprAST> Cond, Then, Else;
+  std::unique_ptr<ExprAST> Cond;
+  std::vector<std::unique_ptr<ExprAST>> Then, Else;
 
 public:
-  IfExprAST(std::unique_ptr<ExprAST> Cond, std::unique_ptr<ExprAST> Then,
-            std::unique_ptr<ExprAST> Else)
+  IfExprAST(std::unique_ptr<ExprAST> Cond, std::vector<std::unique_ptr<ExprAST>> Then,
+            std::vector<std::unique_ptr<ExprAST>> Else)
       : Cond(std::move(Cond)), Then(std::move(Then)), Else(std::move(Else)) {}
 
     llvm::Value *codegen() override;
