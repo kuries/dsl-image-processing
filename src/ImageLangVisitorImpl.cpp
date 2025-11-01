8e9ff98c789a81b3a5279ec80f2ff41d13eea1e2
kuries <bineshmunukurthi@gmail.com>
2025-11-01 12:16:48 +0530

    Add backend support for FOR

--- src/ImageLangVisitorImpl.cpp
+++ src/ImageLangVisitorImpl.cpp
@@ -348,7 +348,89 @@ llvm::Value *IfExprAST::codegen() {
 }
 
 
-llvm::Value* ForExprAST::codegen() {
-    // TODO: implement codegen
-    return nullptr;
+llvm::Value *ForExprAST::codegen() {
+	llvm::Function *TheFunction = Builder.GetInsertBlock()->getParent();
+
+	// Create an alloca for the variable in the entry block.
+	llvm::AllocaInst *Alloca = Helper::CreateEntryBlockAlloca(TheFunction, VarName);
+
+	// Emit the start code first, without 'variable' in scope.
+	llvm::Value *StartVal = Start->codegen();
+	if (!StartVal)
+	return nullptr;
+
+	// Store the value into the alloca.
+	Builder.CreateStore(StartVal, Alloca);
+
+	// Make the new basic block for the loop header, inserting after current
+	// block.
+	llvm::BasicBlock *LoopBB = llvm::BasicBlock::Create(TheContext, "loop", TheFunction);
+
+	// Insert an explicit fall through from the current block to the LoopBB.
+	Builder.CreateBr(LoopBB);
+
+	// Start insertion in LoopBB.
+	Builder.SetInsertPoint(LoopBB);
+
+	// Within the loop, the variable is defined equal to the PHI node.  If it
+	// shadows an existing variable, we have to restore it, so save it now.
+	llvm::AllocaInst *OldVal = NamedValues[VarName];
+	NamedValues[VarName] = Alloca;
+
+	// Emit the body of the loop.  This, like any other expr, can change the
+	// current BB.  Note that we ignore the value computed by the body, but don't
+	// allow an error.
+	if(Body.empty())
+		return nullptr;
+	for(int i=0; i<Body.size(); i++){
+		llvm::Value* ret = Body[i]->codegen();
+		if(!ret)
+			return nullptr;
+	}
+	
+
+	// Emit the step value.
+	llvm::Value *StepVal = nullptr;
+	if (Step) {
+		StepVal = Step->codegen();
+	if (!StepVal)
+		return nullptr;
+	} else {
+	// If not specified, use 1.0.
+		StepVal = llvm::ConstantFP::get(TheContext, llvm::APFloat(1.0));
+	}
+
+	// Compute the end condition.
+	llvm::Value *EndCond = End->codegen();
+	if (!EndCond)
+		return nullptr;
+
+	// Reload, increment, and restore the alloca.  This handles the case where
+	// the body of the loop mutates the variable.
+	llvm::Value *CurVar =
+		Builder.CreateLoad(Alloca->getAllocatedType(), Alloca, VarName.c_str());
+	llvm::Value *NextVar = Builder.CreateFAdd(CurVar, StepVal, "nextvar");
+	Builder.CreateStore(NextVar, Alloca);
+
+	// i<End
+	EndCond = Builder.CreateFCmpULT(NextVar, EndCond, "cond");
+
+	// Create the "after loop" block and insert it.
+	llvm::BasicBlock *AfterBB =
+		llvm::BasicBlock::Create(TheContext, "afterloop", TheFunction);
+
+	// Insert the conditional branch into the end of LoopEndBB.
+	Builder.CreateCondBr(EndCond, LoopBB, AfterBB);
+
+	// Any new code will be inserted in AfterBB.
+	Builder.SetInsertPoint(AfterBB);
+
+	// Restore the unshadowed variable.
+	if (OldVal)
+		NamedValues[VarName] = OldVal;
+	else
+		NamedValues.erase(VarName);
+
+	// for expr always returns 0.0.
+	return llvm::Constant::getNullValue(llvm::Type::getDoubleTy(TheContext));
 }
